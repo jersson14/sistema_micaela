aac3c14cca8dea6b5cda12b0b814d87154702541
Jersson <jersson1407miranda@gmail.com>
2025-11-22 13:30:23 -0500

    suben

--- controller/usuario/controlador_iniciar_sesion.php
+++ controller/usuario/controlador_iniciar_sesion.php
@@ -1,11 +1,25 @@
 <?php
-    require '../../model/model_usuario.php';
-    require '../../utilitario/JWTHelper.php';
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
     
-    $MU = new Modelo_Usuario();
-    $usu = htmlspecialchars($_POST['u'],ENT_QUOTES,'UTF-8');
-    $con = htmlspecialchars($_POST['c'],ENT_QUOTES,'UTF-8');
-    $consulta = $MU->Verificar_Usuario($usu,$con);
+    try {
+        require '../../model/model_usuario.php';
+        require '../../utilitario/JWTHelper.php';
+        
+        // Verificar que lleguen los datos POST
+        if (!isset($_POST['u']) || !isset($_POST['c'])) {
+            echo json_encode(array('success' => false, 'message' => 'Faltan datos de usuario o contraseña'));
+            exit;
+        }
+        
+        $MU = new Modelo_Usuario();
+        $usu = htmlspecialchars($_POST['u'],ENT_QUOTES,'UTF-8');
+        $con = htmlspecialchars($_POST['c'],ENT_QUOTES,'UTF-8');
+        $consulta = $MU->Verificar_Usuario($usu,$con);
+    } catch (Exception $e) {
+        echo json_encode(array('success' => false, 'message' => 'Error: ' . $e->getMessage()));
+        exit;
+    }
     
     if(count($consulta)>0){
         // Generar tokens JWT
