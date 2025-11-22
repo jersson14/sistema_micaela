8f108106a2d2eb82cb4a7de399027242814a485b
Jersson <jersson1407miranda@gmail.com>
2025-11-22 13:47:17 -0500

    Disable JWT for production compatibility

--- controller/usuario/controlador_iniciar_sesion.php
+++ controller/usuario/controlador_iniciar_sesion.php
@@ -4,7 +4,8 @@
     
     try {
         require '../../model/model_usuario.php';
-        require '../../utilitario/JWTHelper.php';
+        // JWT desactivado temporalmente para producción
+        // require '../../utilitario/JWTHelper.php';
         
         // Verificar que lleguen los datos POST
         if (!isset($_POST['u']) || !isset($_POST['c'])) {
@@ -22,40 +23,9 @@
     }
     
     if(count($consulta)>0){
-        // Generar tokens JWT
-        $userData = array(
-            'id_usuario' => $consulta[0][0],
-            'dni_usuario' => $consulta[0][1],
-            'nombre' => $consulta[0][2],
-            'nombres_completos' => $consulta[0][4],
-            'usuario' => $consulta[0][7],
-            'id_role' => $consulta[0][9],
-            'foto' => $consulta[0][15],
-            'foto_empresa' => $consulta[0][17],
-            'razon' => $consulta[0][18],
-            'nombre_rol' => $consulta[0][19],
-            'sucursal' => $consulta[0][20]
-        );
-        
-        // Generar access token (2 horas)
-        $accessToken = JWTHelper::generateToken($userData, 2);
-        
-        // Generar refresh token (7 días)
-        $refreshToken = JWTHelper::generateRefreshToken($userData);
-        
-        // Agregar tokens a la respuesta
-        $response = array(
-            'success' => true,
-            'data' => $consulta[0],
-            'tokens' => array(
-                'access_token' => $accessToken,
-                'refresh_token' => $refreshToken,
-                'expires_in' => 7200 // 2 horas en segundos
-            )
-        );
-        
-        echo json_encode($response);
+        // Respuesta simple sin JWT (modo compatible)
+        echo json_encode($consulta);
     }else{
-        echo json_encode(array('success' => false, 'message' => 'Credenciales incorrectas'));
+        echo json_encode(array());
     }
 ?>
